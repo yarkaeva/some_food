44da9b1a042abaae3dceb38e00a1e7e46040011b
Darina Y <darina.yarkaeva@gmail.com>
2023-05-10 10:58:04 +0500

    Deleted unused route.

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:some_food/feature/presentation/auth/pages/auth_page.dart';
 import 'package:some_food/feature/presentation/auth/pages/login_page.dart';
-import 'package:some_food/feature/presentation/auth/pages/register_details_page.dart';
 import 'package:some_food/feature/presentation/auth/pages/register_page.dart';
 
 final GoRouter router = GoRouter(
@@ -17,18 +16,6 @@ final GoRouter router = GoRouter(
         GoRoute(
           path: 'register_page',
           builder: (context, state) => const RegisterPage(),
-          routes: [
-            GoRoute(
-              path: 'details',
-              builder: (context, state) {
-                final args = state.extra as Map<String, String>;
-                return RegisterDetailsPage(
-                  email: args.keys.toList().first,
-                  password: args.values.toList().first,
-                );
-              },
-            ),
-          ],
         ),
       ],
     ),
